1f1b264f03d06d0432b13cdbabb033e6516a777d
Jarek Pelczynski <jpelczynski@gmail.com>
2020-01-31 15:18:49 +0100

    Files organize

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { fetchData } = require('./fetchData');
+const { fetchData } = require('./src/fetchData');
 
 const app = express();
 const PORT = 8080;
@@ -8,7 +8,7 @@ const HOST = '0.0.0.0';
 const main = async () => {
   const data = await fetchData();
 
-  app.get('/raw-data', (req, res) => res.json(data));
+  app.get('/', (req, res) => res.json(data));
   app.listen(PORT, HOST);
   console.log(`Running on http://${HOST}:${PORT}`);
 };
