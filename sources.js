63b26ef401df219099dcedafa45e9d27ffc49f71
Jarek Pelczynski <jpelczynski@gmail.com>
2020-02-16 17:34:48 +0100

    Fix resources path

--- sources.js
+++ sources.js
@@ -1,8 +1,8 @@
-const SOURCE_PREFIX = 'https://raw.githubusercontent.com/CSSEGISandData/2019-nCoV/master/time_series';
+const SOURCE_PREFIX = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/';
 const SOURCES = {
-  CONFIRMED: `${SOURCE_PREFIX}/time_series_2019-ncov-Confirmed.csv`,
-  DEATHS: `${SOURCE_PREFIX}/time_series_2019-ncov-Deaths.csv`,
-  RECOVERED: `${SOURCE_PREFIX}/time_series_2019-ncov-Recovered.csv`,
+  CONFIRMED: `${SOURCE_PREFIX}/time_series_19-covid-Confirmed.csv`,
+  DEATHS: `${SOURCE_PREFIX}/time_series_19-covid-Deaths.csv`,
+  RECOVERED: `${SOURCE_PREFIX}/time_series_19-covid-Recovered.csv`,
 }
 
 module.exports = SOURCES;
